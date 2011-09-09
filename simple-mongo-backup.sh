dc8b32ceda5681cfc2938b1113ad15a128c1f22c
Michele Zonca <shatsar@gmail.com>
2011-09-10 01:42:08 +0200

    files archived in tar.gz

--- simple-mongo-backup.sh
+++ simple-mongo-backup.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-# CONFIGURATION
-DBS=”db1 db2”
-DUMP_LOCATION=~/backup
+
+#--
+# CONFIG
+DBS="mashape"
+DUMP_LOCATION=~/backup/mongo
+LOGS_LOCATION=~/backup/logs
 DIR_NAME=db-`date +%Y%m%d`
 DIR_NAME_ABS=$DUMP_LOCATION/$DIR_NAME
 COPY_SSH_DEST=server1:$DIR_NAME_ABS
-##—
+##--
 
-echo “Starting backup `date`”
+echo "Starting backup `date`"
 for DB in $DBS
 do
-	echo “Backup: $DB”
-	mongodump —db $DB —out $DIR_NAME_ABS
+	echo "Backup: $DB"
+	mongodump --db $DB --out $DIR_NAME_ABS
+	tar -czf $DUMP_LOCATION/$DB-`date +%Y%m%d`.tgz --remove-files --directory $DUMP_LOCATION $DIR_NAME
+	scp	$DUMP_LOCATION/$DB-`date +%Y%m%d`.tgz $COPY_SSH_DEST
 done
-
-scp -r $DIR_NAME_ABS $COPY_SSH_DEST
-echo “Backup Finished `date`”
+echo "Backup Finished"
