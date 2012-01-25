ec8e5186a44807f725417f9f83da3abd6fe09603
Michele Zonca <shatsar@gmail.com>
2012-01-25 10:20:26 +0100

    logging

--- simple-mongo-backup.sh
+++ simple-mongo-backup.sh
@@ -6,8 +6,10 @@ DBS="mashape"
 DUMP_LOCATION=~/backup/mongo
 LOGS_LOCATION=~/backup/logs
 DIR_NAME=db-`date +%Y%m%d`
+LOG_FILE=$DUMP_LOCATION/log-`date +%Y%m%d`
 DIR_NAME_ABS=$DUMP_LOCATION/$DIR_NAME
 COPY_SSH_DEST=server1:$DIR_NAME_ABS
+# Uncomment and configure
 #USERNAME="username"
 #PASSWORD="password"
 ##--
@@ -19,13 +21,13 @@ if [ -n "$USERNAME" ]
 	AUTH=" -u $USERNAME -p $PASSWORD";
 fi
 
-echo "Starting backup `date`"
+echo "Starting backup `date`" >> $LOG_FILE
 
 for DB in $DBS
 do
-	echo "Backup: $DB"
-	mongodump --db $DB $AUTH --out $DIR_NAME_ABS
-	tar -czf $DUMP_LOCATION/$DB-`date +%Y%m%d`.tgz --remove-files --directory $DUMP_LOCATION $DIR_NAME
-	scp	$DUMP_LOCATION/$DB-`date +%Y%m%d`.tgz $COPY_SSH_DEST
+	echo "Backup: $DB" >> $LOG_FILE
+	mongodump --db $DB $AUTH --out $DIR_NAME_ABS >> $LOG_FILE
+	tar -czf $DUMP_LOCATION/$DB-`date +%Y%m%d`.tgz --remove-files --directory $DUMP_LOCATION $DIR_NAME >> $LOG_FILE
+	scp	$DUMP_LOCATION/$DB-`date +%Y%m%d`.tgz $COPY_SSH_DEST >> $LOG_FILE
 done
-echo "Backup Finished"
+echo "Backup Finished" >> $LOG_FILE
